ecd654ff074ca4d4d75cba00e044375b3a01ae8b
kiraahmad <60839257+kiraahmad@users.noreply.github.com>
2020-10-25 21:19:21 +0800

    editing tasks graddbing query to accept completed or not completed tasks

--- src/models/task.js
+++ src/models/task.js
@@ -19,5 +19,5 @@ const taskSchema = new mongoose.Schema({
     timestamps: true
 });
 
-const Task = mongoose.model('Task', taskSchema);
+const Task = mongoose.model('Tasks', taskSchema);
 module.exports = Task
