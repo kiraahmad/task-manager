e5a4ef2f862d98315c061094829f2810075395e9
kiraahmad <60839257+kiraahmad@users.noreply.github.com>
2020-10-31 22:13:47 +0800

    adding multer for supporting files uploads

--- src/app.js
+++ src/app.js
@@ -6,17 +6,16 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if(req.method === 'POST') {
-//         res.send('Sign ups are currently disabled!');
-//     } else {
-//         next();
-//     }
-// });
+const multer = require('multer');
+const upload = multer({
+    dest: 'images'
+});
 
-// app.use((req,res,next) => {
-//     res.status(503).send('Site is currently under maintenance, Please try again soon.');
-// });
+app.post('/upload', upload.single('upload'), (req,res) => {
+    res.send()
+})
+
+
 
 app.use(express.json());
 app.use(userRouter);
@@ -26,17 +25,3 @@ app.use(taskRouter);
 app.listen(port, ()=> {
     console.log('server is running on port ' + port);
 });
-
-const Task = require('./models/task');
-const User = require('./models/user');
-
-const main = async () => {
-    // const task = await Task.findById('5f93c38be612103064c94eaf');
-    // await task.populate('owner').execPopulate();
-    // console.log(task.owner);
-
-    // const user = await User.findById('5f93c21ec14aac2904bcca9e');
-    // await user.populate('tasks').execPopulate()
-    // console.log(user.tasks)
-}
-main()
