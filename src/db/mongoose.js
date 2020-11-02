5cf653502ea9afea5b61828ece1a5f4340fa8053
kiraahmad <60839257+kiraahmad@users.noreply.github.com>
2020-11-03 03:16:05 +0800

    fixed error

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`${process.env.MONOGDB_URL}`, {
+mongoose.connect(process.env.MONOGDB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
