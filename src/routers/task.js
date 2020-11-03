74687abfe03b71346acdcaea2205b381c6b97f3f
kiraahmad <60839257+kiraahmad@users.noreply.github.com>
2020-11-03 16:16:53 +0800

    edited tasks routes

--- src/routers/task.js
+++ src/routers/task.js
@@ -67,7 +67,7 @@ router.patch('/tasks/:id',auth, async (req,res) => {
     const isValidUpdate = updates.every((update) => allowedUpdates.includes(update));
 
     if(!isValidUpdate) {
-        res.status(400).send({error: 'Invalid updates!'});
+        return res.status(400).send({error: 'Invalid updates!'});
     }
     try {
         const task = await Task.findOne({_id: req.params.id, owner:req.user._id})
@@ -79,16 +79,15 @@ router.patch('/tasks/:id',auth, async (req,res) => {
         await task.save();
         res.send(task);
     } catch(e) {
-        res.status(500).send(e)
+        res.status(400).send(e)
     }
 });
 
 router.delete('/tasks/:id', auth, async (req,res) => {
     try {
         const task = await Task.findOneAndDelete({_id: req.params.id, owner: req.user.id});
-        // const task = await Task.findByIdAndDelete(req.params.id);
         if(!task) {
-            return res.status(404).send()
+            res.status(404).send()
         }
 
         res.send(task)
