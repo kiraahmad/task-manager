923cf4d3e8e2a3ae1d654cc87b8a728095e67d14
kiraahmad <60839257+kiraahmad@users.noreply.github.com>
2020-11-03 18:22:01 +0800

    returned the welcoming email function back

--- src/routers/user.js
+++ src/routers/user.js
@@ -10,8 +10,8 @@ router.post('/users', async (req, res) => {
     const user = new User(req.body);
     try {
         await user.save();
-        // sendWelcomeEmail(user.email, user.name);
         const token = await user.generateAuthToken();
+        sendWelcomeEmail(user.email, user.name);
         res.status(201).send({user, token});
     } catch(e) {
         res.status(400).send(e)
